61e4df6651be02ce8b5748a514882e1e2bb971ea
Loisseux <ton-email-loisantoinee@gmail.com>
2026-05-27 03:59:48 +0200

    Ajout Capacitor iOS et Android

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.flirtcoach.app',
+  appName: 'FlirtCoach',
+  webDir: 'dist'
+};
+
+export default config;
